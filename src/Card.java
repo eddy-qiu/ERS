a86ed4066a13821165c57208fb2a8fdc7531beab
eddy-qiu <eqiu22@gmail.com>
2022-03-25 08:43:54 -0400

    created Card class

---
+++ src/Card.java
@@ -0,0 +1,20 @@
+
+public class Card {
+	int value;
+	boolean face = false;
+	Card(int assignedValue){
+		if(assignedValue < 1 || assignedValue > 13) {
+		throw new Exception("invalid card");
+		}
+		value = assignedValue;
+		if(assignedValue > 10 || assignedValue == 1) {
+			face = true;
+		}
+	}
+	public int getValue() {
+		return value;
+	}
+	public boolean isFace() {
+		return face;
+	}
+}
