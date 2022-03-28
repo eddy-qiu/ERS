279f358d8a8e47be49cd03d1449c1f6f624a51ba
eddy-qiu <eqiu22@gmail.com>
2022-03-28 08:50:44 -0400

    isSlap and faceSequence

--- src/CenterPile.java
+++ src/CenterPile.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class CenterPile extends Deck{
-	ArrayList<Card> Cards = new ArrayList<Cards>;
+	ArrayList<Card> Cards = new ArrayList<Card>;
+	private int faceRemaining = 0;
 	public Card get(int index) {
 		Cards.get(index);
 	}
@@ -12,9 +13,41 @@ public class CenterPile extends Deck{
 		Cards.remove(index);
 	}
 	public boolean isSlap() {
-		
+		if(Cards.get(Cards.size()).getValue() == Cards.get(Cards.size()-1).getValue()) {
+			return true;
+		}
+		if(Cards.get(Cards.size()).getValue() == Cards.get(size()-2).getValue()) {
+			return true;
+		}
+		if((Cards.get(Cards.size()).getValue() == 12 && Cards.get(Cards.size()-1).getValue == 13) || (Cards.get(Cards.size()).getValue() == 13 && Cards.get(Cards.size()-1).getValue == 12)) {
+			return true;
+		}
+		return false;
 	}
 	public int faceSequence() {
-		
+		if(faceRemaining != 0) {
+			faceRemaining -= 1;
+			return faceRemaining + 1;
+		}
+		else {
+			if(Cards.get(Cards.size()).getValue() == 1) {
+				faceRemaining = 4;
+				return 4;
+			}
+			if(Cards.get(Cards.size()).getValue() == 13) {
+				faceRemaining = 3;
+				return 3;
+			}
+			if(Cards.get(Cards.size()).getValue() == 12) {
+				faceRemaining = 2;
+				return 2;
+			}
+			if(Cards.get(Cards.size()).getValue() == 11) {
+				faceRemaining = 1;
+				return 1;
+			}
+			faceRemaining = 0;
+			return 0;
+		}
 	}
 }
