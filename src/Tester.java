35a44e8bbec534c9be51335d44f486586bd7401b
eddy-qiu <eqiu22@gmail.com>
2022-03-30 08:57:23 -0400

    CenterPile testing: WORKS!

--- src/Tester.java
+++ src/Tester.java
@@ -7,6 +7,29 @@ public class Tester {
 		System.out.println(a.get(10).value);
 		a.shuffle();
 		System.out.println(a.get(1).value);
+		
+		CenterPile b = new CenterPile();
+		System.out.println(b.isSlap());
+		b.add(new Card(13));
+		b.add(new Card(3));
+		b.add(new Card(3));
+		System.out.println(b.isSlap());
+		b.add(new Card(2));
+		System.out.println(b.isSlap());
+		b.add(new Card(3));
+		System.out.println(b.isSlap());
+		b.add(new Card(13));
+		b.add(new Card(12));
+		System.out.println(b.isSlap());
+		b.add(new Card(12));
+		b.add(new Card(13));
+		System.out.println(b.isSlap());
+		System.out.println(b.faceSequence());
+		System.out.println(b.faceSequence());
+		System.out.println(b.faceSequence());
+		System.out.println(b.faceSequence());
+		System.out.println(b.faceSequence());
+		System.out.println(b.faceSequence());
 	}
 	
 }
