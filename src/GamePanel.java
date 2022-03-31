c4c3271b4a4c30230cb51edafc68d7ba950c2b21
eddy-qiu <eqiu22@gmail.com>
2022-03-31 08:37:38 -0400

    created gamepanel

---
+++ src/GamePanel.java
@@ -0,0 +1,12 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements ActionListener{
+	JFrame GameFrame;
+
+	public void actionPerformed(ActionEvent e) {
+		// use this to register player moves
+		
+	}
+	
+}
