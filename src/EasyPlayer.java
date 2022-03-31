5791e3132b17bf2fb07966f941138fa55238e8fa
eddy-qiu <eqiu22@gmail.com>
2022-03-31 09:04:13 -0400

    ComputerPlayer template made

--- src/EasyPlayer.java
+++ src/EasyPlayer.java
@@ -1,4 +1,19 @@
+import java.util.concurrent.TimeUnit;
 
-public class EasyPlayer {
-
+public class EasyPlayer { //general template for computer player, might make the easy player misslap sometimes
+	public String getMove(CenterPile pile, PlayerPile myPile) throws InterruptedException {
+		if(myPile.length() != 0 || myPile.length() > 0) {
+			if(pile.isSlap()) {
+				TimeUnit.SECONDS.sleep(1);
+				return "Slap";
+			}
+			else {
+				TimeUnit.SECONDS.sleep(3/2);
+				return "Play";
+			}
+		}
+		else {
+			return null;
+		}
+	}
 }
