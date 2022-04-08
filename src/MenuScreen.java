6754356516959f8dbfc74a1325f749671d9c0795
sanusu0139 <anusuri.sairishi@gmail.com>
2022-04-08 08:38:54 -0400

    More changes to menuScreen

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -4,12 +4,12 @@ import java.awt.*;
 public class MenuScreen extends JPanel implements ActionListener{
 
 	JFrame mainFrame, tutorialFrame;
-	JPanel menuPanel, tutorialPanel, buttonPanel, textPanel;
+	JPanel menuPanel, menuTextPanel, tutorialPanel, tutorialbuttonPanel, tutorialTextPanel, menubuttonPanel;
 	JButton start, tutorialButton, nextPage, backToMain;
 	JLabel titleText, tutorialText;
 	private int tutorialPage = 1;
 
-	public MenuScreen() {
+	public MenuScreen(){
 		//Main Panel
 		mainFrame = new JFrame("Egyptian Rat Screw");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,19 +21,36 @@ public class MenuScreen extends JPanel implements ActionListener{
 		titleText = new JLabel("Egyptian Rat Screw");
 		titleText.setAlignmentX(CENTER_ALIGNMENT);
 		titleText.setAlignmentY(TOP_ALIGNMENT);
-		menuPanel.add(titleText);
+//		menuPanel.add(titleText);
 		
 		start = new JButton("Start Game");
-		start.setAlignmentX(CENTER_ALIGNMENT);
+		start.setAlignmentX(RIGHT_ALIGNMENT);
 		start.setAlignmentY(TOP_ALIGNMENT);
 		start.addActionListener(this);
-		menuPanel.add(start);
+//		menuPanel.add(start);
 		
 		tutorialButton = new JButton("Tutorial");
-		tutorialButton.setAlignmentX(CENTER_ALIGNMENT);
+		tutorialButton.setAlignmentX(RIGHT_ALIGNMENT);
 		tutorialButton.setAlignmentY(TOP_ALIGNMENT);
 		tutorialButton.addActionListener(this);
-		menuPanel.add(tutorialButton);
+		//menuPanel.add(tutorialButton);
+		
+		menuTextPanel = new JPanel();
+		menuTextPanel.setLayout(new BoxLayout(menuTextPanel, BoxLayout.PAGE_AXIS));
+		menuTextPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
+		menuTextPanel.add(titleText);
+		menuPanel.add(menuTextPanel, BorderLayout.CENTER);
+		
+		menubuttonPanel = new JPanel();
+		menubuttonPanel.setLayout(new BoxLayout(menubuttonPanel, BoxLayout.LINE_AXIS));
+		menubuttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
+		menubuttonPanel.add(start);
+		menubuttonPanel.add(Box.createHorizontalGlue());
+		menubuttonPanel.add(tutorialButton);
+		menuPanel.add(menubuttonPanel, BorderLayout.PAGE_END);
+		
+		
+		
 		
 		
 		//Tutorial Panel
@@ -44,41 +61,43 @@ public class MenuScreen extends JPanel implements ActionListener{
 		tutorialPanel.setLayout(new BoxLayout
 				(tutorialPanel, BoxLayout.PAGE_AXIS));
 		
-		textPanel = new JPanel();
-		textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.PAGE_AXIS));
-		textPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
-		
-		buttonPanel = new JPanel();
-		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
-		buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
-		buttonPanel.add(Box.createHorizontalGlue());
-		
-		tutorialPanel.add(buttonPanel, BorderLayout.PAGE_END);
-		
 		backToMain = new JButton("Back to menu");
-		backToMain.setAlignmentX(CENTER_ALIGNMENT);
-		backToMain.setAlignmentY(TOP_ALIGNMENT);
+//		backToMain.setAlignmentX(CENTER_ALIGNMENT);
+//		backToMain.setAlignmentY(TOP_ALIGNMENT);
 		backToMain.addActionListener(this);
-		tutorialPanel.add(backToMain);
+		
 	
 		nextPage = new JButton("Next");
 //		nextPage.setAlignmentX(CENTER_ALIGNMENT);
 //		nextPage.setAlignmentY(TOP_ALIGNMENT);
 		nextPage.addActionListener(this);
-		buttonPanel.add(nextPage);
 		
 		tutorialText = new JLabel("First Page");
-		tutorialText.setPreferredSize(new Dimension(200, 40));
-		tutorialText.setAlignmentX(CENTER_ALIGNMENT);
+		tutorialText.setPreferredSize(new Dimension(720, 50));
+		tutorialText.setAlignmentX(LEFT_ALIGNMENT);
 //		tutorialText.setAlignmentY(TOP_ALIGNMENT);
-		tutorialPanel.add(tutorialText);
+		
+		tutorialTextPanel = new JPanel();
+		tutorialTextPanel.setLayout(new BoxLayout(tutorialTextPanel, BoxLayout.PAGE_AXIS));
+		tutorialTextPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
+		tutorialTextPanel.add(tutorialText);
+		tutorialPanel.add(tutorialTextPanel, BorderLayout.CENTER);
+		
+		tutorialbuttonPanel = new JPanel();
+		tutorialbuttonPanel.setLayout(new BoxLayout(tutorialbuttonPanel, BoxLayout.LINE_AXIS));
+		tutorialbuttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
+//		buttonPanel.setSize(720, 360);
+		tutorialbuttonPanel.add(backToMain);
+		tutorialbuttonPanel.add(Box.createHorizontalGlue());
+		tutorialbuttonPanel.add(nextPage);
+		tutorialPanel.add(tutorialbuttonPanel, BorderLayout.PAGE_END);
 		
 		menuPanel.setBorder(BorderFactory.
 				createEmptyBorder(360, 360, 360, 360));
-		mainFrame.setSize(720, 720);
+//		mainFrame.setSize(720, 720);
 		tutorialPanel.setBorder(BorderFactory.
-				createEmptyBorder(360, 360, 360, 360));
-		tutorialFrame.setSize(720, 720);
+				createEmptyBorder(30, 30, 30, 30));
+//		tutorialPanel.setSize(720, 720);
 		mainFrame.setContentPane(menuPanel);
 		mainFrame.pack();
 		
