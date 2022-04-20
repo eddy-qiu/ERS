17093d05797790d293b7e46a0b8e5a800bd5ba60
eddy-qiu <eqiu22@gmail.com>
2022-04-20 08:57:00 -0400

    will copy over MenuScreen after main components of it are finished

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,21 @@
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class GamePanel extends JPanel implements ActionListener{
 	JFrame GameFrame;
 	PlayerPile players[];
-
+	JLayeredPane centerPile;
+	public GamePanel() {
+		GameFrame = new JFrame();
+		GameFrame.setPreferredSize(new Dimension(720,720));
+		GameFrame.setLayout(new GridLayout(3,3));
+		centerPile = new JLayeredPane();
+		centerPile.setPreferredSize(new Dimension(240,240));
+		
+		
+	}
 	public GamePanel(int numPlayers) throws Exception{//remember that animations can be separate from some backend functionality
 		players = new PlayerPile[numPlayers+1];
 		FullDeck allCards = new FullDeck(); //create 52 cards, shuffle them into respective number of player decks
