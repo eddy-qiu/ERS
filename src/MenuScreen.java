e8dc1ca79487341df92da3f7454070f5a6f80df8
sanusu0139 <anusuri.sairishi@gmail.com>
2022-04-20 09:00:58 -0400

    changed menuscreen

--- src/MenuScreen.java
+++ src/MenuScreen.java
@@ -5,9 +5,9 @@ public class MenuScreen extends JPanel implements ActionListener{
 
 	JFrame mainFrame, tutorialFrame;
 	JPanel menuPanel, menuTextPanel, tutorialPanel, 
-	tutorialbuttonPanel, tutorialTextPanel, menubuttonPanel;
+	tutorialbuttonPanel, tutorialTextPanel, menuButtonPanel;
 	JButton start, tutorialButton, nextPage, backToMain;
-	JLabel titleText, tutorialText, card, card2, card3, card4, empty1, empty2, empty3;
+	JLabel titleText, tutorialText, topCard, leftCard, rightCard, bottomCard, empty1, empty2, empty3;
 	private int tutorialPage = 1;
 
 	public MenuScreen(){
@@ -16,71 +16,60 @@ public class MenuScreen extends JPanel implements ActionListener{
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		menuPanel = new JPanel();
 		menuPanel.setBackground(Color.YELLOW);
-		menuPanel.setLayout(new GridLayout(3,3, 10, 10));
-		
+		menuPanel.setLayout(new GridLayout(3,3, 10, 20));
+
 		titleText = new JLabel("Egyptian Rat Screw");
 		titleText.setAlignmentX(CENTER_ALIGNMENT);
-		titleText.setAlignmentY(TOP_ALIGNMENT);
-//		menuPanel.add(titleText);
-		
+
 		start = new JButton("Start Game");
-		start.setAlignmentX(RIGHT_ALIGNMENT);
-		start.setAlignmentY(TOP_ALIGNMENT);
+		start.setAlignmentX(CENTER_ALIGNMENT);
+		start.setAlignmentY(BOTTOM_ALIGNMENT);
 		start.addActionListener(this);
-//		menuPanel.add(start);
-		
+
 		tutorialButton = new JButton("Tutorial");
 		tutorialButton.setAlignmentX(RIGHT_ALIGNMENT);
 		tutorialButton.setAlignmentY(TOP_ALIGNMENT);
 		tutorialButton.addActionListener(this);
-		//menuPanel.add(tutorialButton);
 		empty1 = new JLabel(" ");
 		empty2 = new JLabel(" ");
 		empty3 = new JLabel(" ");
-		
-		ImageIcon card1 = new ImageIcon("cards/cardBack.png");
-		Image image1 = card1.getImage();
-		Image newimg = image1.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
-		card1 = new ImageIcon(newimg);  // transform it back
-		card = new JLabel(card1);
-		card2 = new JLabel(card1);
-		card3 = new JLabel(card1);
-		card4 = new JLabel(card1);
-		
+
+		ImageIcon cardBack = new ImageIcon("cards/cardBack.png");
+		Image image1 = cardBack.getImage();
+		Image newimg = image1.getScaledInstance(120, 160,  java.awt.Image.SCALE_SMOOTH);  
+		cardBack = new ImageIcon(newimg);
+		topCard = new JLabel(cardBack);
+		leftCard = new JLabel(cardBack);
+		rightCard = new JLabel(cardBack);
+		bottomCard = new JLabel(cardBack);
+
 		menuTextPanel = new JPanel();
-		menuTextPanel.setLayout(new BoxLayout(menuTextPanel, BoxLayout.PAGE_AXIS));
+		menuTextPanel.setLayout(new BoxLayout(menuTextPanel, BoxLayout.Y_AXIS));
 		menuTextPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
 		menuTextPanel.add(titleText);
-//		menuPanel.add(menuTextPanel);
-		
-		
-		
-		
-		menubuttonPanel = new JPanel();
-		menubuttonPanel.setLayout(new BoxLayout(menubuttonPanel, BoxLayout.LINE_AXIS));
-		menubuttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
-		menubuttonPanel.add(start);
-		menubuttonPanel.add(Box.createHorizontalGlue());
-		menubuttonPanel.add(tutorialButton);
-//		menuPanel.add(menubuttonPanel);
-		
+		menuTextPanel.add(start);
+
+		menuButtonPanel = new JPanel();
+		menuButtonPanel.setLayout(new BoxLayout(menuButtonPanel, BoxLayout.Y_AXIS));
+		menuButtonPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
+		menuButtonPanel.setBackground(Color.YELLOW);
+		menuButtonPanel.add(Box.createVerticalGlue());
+		menuButtonPanel.add(tutorialButton);
+
 		menuPanel.add(empty1);
-		
-		menuPanel.add(card);
-		
+		menuPanel.add(topCard);
 		menuPanel.add(empty2);
-		
-		menuPanel.add(card2);
-		
+		menuPanel.add(leftCard);
 		menuPanel.add(menuTextPanel);
-		
-		menuPanel.add(card3);
-		
+		menuPanel.add(rightCard);
 		menuPanel.add(empty3);
-		
-		menuPanel.add(card4);
-		
-		menuPanel.add(menubuttonPanel);
+		menuPanel.add(bottomCard);
+		menuPanel.add(menuButtonPanel);
+
+
+
+
+
 		//Tutorial Panel
 		tutorialFrame = new JFrame("Tutorial");
 		tutorialFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -88,47 +77,47 @@ public class MenuScreen extends JPanel implements ActionListener{
 		tutorialPanel.setBackground(Color.GREEN);
 		tutorialPanel.setLayout(new BoxLayout
 				(tutorialPanel, BoxLayout.PAGE_AXIS));
-		
+
 		backToMain = new JButton("Back to menu");
-//		backToMain.setAlignmentX(CENTER_ALIGNMENT);
-//		backToMain.setAlignmentY(TOP_ALIGNMENT);
+		//		backToMain.setAlignmentX(CENTER_ALIGNMENT);
+		//		backToMain.setAlignmentY(TOP_ALIGNMENT);
 		backToMain.addActionListener(this);
-		
-	
+
+
 		nextPage = new JButton("Next");
-//		nextPage.setAlignmentX(CENTER_ALIGNMENT);
-//		nextPage.setAlignmentY(TOP_ALIGNMENT);
+		//		nextPage.setAlignmentX(CENTER_ALIGNMENT);
+		//		nextPage.setAlignmentY(TOP_ALIGNMENT);
 		nextPage.addActionListener(this);
-		
+
 		tutorialText = new JLabel("First Page");
 		tutorialText.setPreferredSize(new Dimension(720, 50));
 		tutorialText.setAlignmentX(LEFT_ALIGNMENT);
-//		tutorialText.setAlignmentY(TOP_ALIGNMENT);
-		
+		//		tutorialText.setAlignmentY(TOP_ALIGNMENT);
+
 		tutorialTextPanel = new JPanel();
 		tutorialTextPanel.setLayout(new BoxLayout(tutorialTextPanel, BoxLayout.PAGE_AXIS));
 		tutorialTextPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
 		tutorialTextPanel.add(tutorialText);
 		tutorialPanel.add(tutorialTextPanel, BorderLayout.CENTER);
-		
+
 		tutorialbuttonPanel = new JPanel();
 		tutorialbuttonPanel.setLayout(new BoxLayout(tutorialbuttonPanel, BoxLayout.LINE_AXIS));
 		tutorialbuttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 10, 20));
-//		buttonPanel.setSize(720, 360);
+		//		buttonPanel.setSize(720, 360);
 		tutorialbuttonPanel.add(backToMain);
 		tutorialbuttonPanel.add(Box.createHorizontalGlue());
 		tutorialbuttonPanel.add(nextPage);
 		tutorialPanel.add(tutorialbuttonPanel, BorderLayout.PAGE_END);
-		
+
 		menuPanel.setBorder(BorderFactory.
 				createEmptyBorder(100, 100, 100, 100));
-//		mainFrame.setSize(720, 720);
+		//		mainFrame.setSize(720, 720);
 		tutorialPanel.setBorder(BorderFactory.
 				createEmptyBorder(30, 30, 30, 30));
-//		tutorialPanel.setSize(720, 720);
+		//		tutorialPanel.setSize(720, 720);
 		mainFrame.setContentPane(menuPanel);
 		mainFrame.pack();
-		
+
 		tutorialFrame.setContentPane(tutorialPanel);
 		tutorialFrame.pack();
 		mainFrame.setVisible(true);
@@ -139,9 +128,9 @@ public class MenuScreen extends JPanel implements ActionListener{
 		if(eventName.equals("Tutorial")) {
 			tutorialFrame.setVisible(true);
 			mainFrame.hide();
-			
+
 		}else if(eventName.equals("Next")) {
-			
+
 			if(tutorialPage == 0) {
 				tutorialText.setText("All 52 cards will be dealt evenly among every player. The player will always starts first  ");
 				tutorialPage++;
