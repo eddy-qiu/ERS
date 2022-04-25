0896e955250458aa5c1ad52cfeb6f8cc7346cb79
eqiu9390 <eqiu9390@CHHB2ZPJXV1.inst.hcpss.org>
2022-04-25 09:10:30 -0400

    making the card show up

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,7 @@
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 
@@ -27,15 +30,23 @@ public class Card {
 		return cardSuite;
 	}
 	public ImageIcon getImage() {
-		String appendedCardName = String.valueOf(this.getValue()) + "_of_"+ this.getSuite() + ".png";
+		String appendedCardName = String.valueOf("cards/"+this.getValue() + "_of_"+ this.getSuite() + ".png");
+		System.out.println(appendedCardName);
 		cardImage = new ImageIcon(appendedCardName);
 		Image image1 = cardImage.getImage();
 		Image newimg1 = image1.getScaledInstance(120,160,java.awt.Image.SCALE_SMOOTH);
 		return new ImageIcon(newimg1);
 	}
 	public ImageIcon getImageSideways() {
+		String appendedCardName = String.valueOf("cards/"this.getValue() + "_of_"+ this.getSuite() + ".png");
+		cardImage = new ImageIcon(appendedCardName);
+		Image image1 = cardImage.getImage();
+		Image newimg1 = image1.getScaledInstance(120,160,java.awt.Image.SCALE_SMOOTH);
+		
+	    BufferedImage image = (BufferedImage)newimg1;
+		
 		// Calculate the new size of the image based on the angle of rotaion
-	    double radians = Math.toRadians(degrees);
+	    double radians = Math.toRadians(90);
 	    double sin = Math.abs(Math.sin(radians));
 	    double cos = Math.abs(Math.cos(radians));
 	    int newWidth = (int) Math.round(image.getWidth() * cos + image.getHeight() * sin);
@@ -52,10 +63,10 @@ public class Card {
 	    at.setToRotation(radians, x + (image.getWidth() / 2), y + (image.getHeight() / 2));
 	    at.translate(x, y);
 	    g2d.setTransform(at);
-	    // Paint the originl image
+	    // Paint the original image
 	    g2d.drawImage(image, 0, 0, null);
 	    g2d.dispose();
-	    return rotate;
-
+	    
+	    return new ImageIcon(rotate);
 	}
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,37 +1,65 @@
-
+import javax.swing.*;
 public class Tester {
-	public static void main(String[] args) throws Exception {
-		FullDeck a  = new FullDeck(); //fulldeck testing
-		System.out.println(a.length());
-		System.out.println(a.get(1).value);
-		System.out.println(a.get(10).value);
-		a.shuffle();
-		System.out.println(a.get(1).value);
+//	public static void main(String[] args) throws Exception {
+//		FullDeck a  = new FullDeck(); //fulldeck testing
+//		System.out.println(a.length());
+//		System.out.println(a.get(1).value);
+//		System.out.println(a.get(10).value);
+//		a.shuffle();
+//		System.out.println(a.get(1).value);
+//		
+//		CenterPile b = new CenterPile(); //centerpile testing
+//		System.out.println(b.isSlap());
+////		b.add(new Card(13));
+////		b.add(new Card(3));
+////		b.add(new Card(3));
+////		System.out.println(b.isSlap());
+////		b.add(new Card(2));
+////		System.out.println(b.isSlap());
+////		b.add(new Card(3));
+////		System.out.println(b.isSlap());
+////		System.out.println(b.faceSequence());
+////		b.add(new Card(13));
+////		b.add(new Card(12));
+////		System.out.println(b.isSlap());
+////		b.add(new Card(12));
+////		b.add(new Card(13));
+////		System.out.println(b.isSlap());
+////		System.out.println(b.faceSequence());
+////		System.out.println(b.faceSequence());
+////		System.out.println(b.faceSequence());
+////		System.out.println(b.faceSequence());
+////		System.out.println(b.faceSequence());
+////		System.out.println(b.faceSequence());
+////		System.out.println(52/3);
 		
-		CenterPile b = new CenterPile(); //centerpile testing
-		System.out.println(b.isSlap());
-		b.add(new Card(13));
-		b.add(new Card(3));
-		b.add(new Card(3));
-		System.out.println(b.isSlap());
-		b.add(new Card(2));
-		System.out.println(b.isSlap());
-		b.add(new Card(3));
-		System.out.println(b.isSlap());
-		System.out.println(b.faceSequence());
-		b.add(new Card(13));
-		b.add(new Card(12));
-		System.out.println(b.isSlap());
-		b.add(new Card(12));
-		b.add(new Card(13));
-		System.out.println(b.isSlap());
-		System.out.println(b.faceSequence());
-		System.out.println(b.faceSequence());
-		System.out.println(b.faceSequence());
-		System.out.println(b.faceSequence());
-		System.out.println(b.faceSequence());
-		System.out.println(b.faceSequence());
-		System.out.println(52/3);
+//	}
+	Tester() throws Exception{
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		Card one = new Card(4, "spades");
+		JLabel oneCard = new JLabel(one.getImage());
+		panel.add(oneCard);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
 	}
-	
+	private static void menuGUI() throws Exception {
+		JFrame.setDefaultLookAndFeelDecorated
+		(true);
+		Tester test = new Tester();
+	}
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.
+		invokeLater(new Runnable() {
+			public void run() {
+				try {
+					menuGUI();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
