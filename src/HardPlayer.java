790d808a3c5cf9ce66a4ce9769a8ecd2efccc418
sanusu0139 <anusuri.sairishi@gmail.com>
2022-05-09 08:51:26 -0400

    added getMove mehod to both players, made hard player faster than medium player

--- src/HardPlayer.java
+++ src/HardPlayer.java
@@ -1,4 +1,19 @@
+import java.util.concurrent.TimeUnit;
 
 public class HardPlayer {
-
+	public String getMove(CenterPile pile, PlayerPile myPile) throws InterruptedException {
+		if(myPile.length() != 0 || myPile.length() > 0) {
+			if(pile.isSlap()) {
+				TimeUnit.MILLISECONDS.sleep(450);
+				return "Slap";
+			}
+			else {
+				TimeUnit.SECONDS.sleep(3/2);
+				return "Play";
+			}
+		}
+		else {
+			return null;
+		}
+	}
 }
--- src/MediumPlayer.java
+++ src/MediumPlayer.java
@@ -1,4 +1,20 @@
+import java.util.concurrent.TimeUnit;
 
 public class MediumPlayer {
-
+	public String getMove(CenterPile pile, PlayerPile myPile) throws InterruptedException {
+		if(myPile.length() != 0 || myPile.length() > 0) {
+			if(pile.isSlap()) {
+				TimeUnit.MILLISECONDS.sleep(750);
+				return "Slap";
+			}
+			else {
+				TimeUnit.SECONDS.sleep(3/2);
+				return "Play";
+			}
+		}
+		else {
+			return null;
+		}
+	}
 }
+
