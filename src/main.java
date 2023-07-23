5e49af09cb9aa635982ea8047b370cf39eaa89af
Eddy Qiu <97773835+eddy-qiu@users.noreply.github.com>
2023-07-23 00:52:58 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,9 +1,7 @@
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hi");
+	
 		
 		MenuScreen Game = new MenuScreen();
 		
